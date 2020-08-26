e2efae259f485b970b8ed8a03126de4a77957bbc
Yehan Pemarathne <yehancha@gmail.com>
2020-08-26 17:35:19 +0530

    Add stream switcher

--- streaming/playback/hlsPlayer.js
+++ streaming/playback/hlsPlayer.js
@@ -1,36 +1,5 @@
-var video = document.getElementById('video');
 var VIDEO_SRC = 'https://cdn3.wowza.com/1/aWtQYmE4K083cUh3/U1FqbUVK/hls/live/playlist.m3u8';
 var AUDIO_SRC = 'https://cdn3.wowza.com/1/S2FaU00wdGFzKzJ0/RFRjWklS/hls/live/playlist.m3u8';
-
-var videoSrc = 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8';
-
-if (Hls.isSupported()) {
-  var hls = new Hls();
-  hls.loadSource(videoSrc);
-  hls.attachMedia(video);
-  hls.on(Hls.Events.MANIFEST_PARSED, function () {
-    document.getElementById('play').removeAttribute('disabled');
-  });
-}
-// hls.js is not supported on platforms that do not have Media Source
-// Extensions (MSE) enabled.
-// 
-// When the browser has built-in HLS support (check using `canPlayType`),
-// we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video
-// element through the `src` property. This is using the built-in support
-// of the plain video element, without using hls.js.
-// 
-// Note: it would be more normal to wait on the 'canplay' event below however
-// on Safari (where you are most likely to find built-in HLS support) the
-// video.src URL must be on the user-driven white-list before a 'canplay'
-// event will be emitted; the last video event that can be reliably
-// listened-for when the URL is not on the white-list is 'loadedmetadata'.
-else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-  video.src = videoSrc;
-  video.addEventListener('loadedmetadata', function () {
-    video.play();
-  });
-}
 
 function copyVideoSrc() {
   copyToClipboard(VIDEO_SRC, 'copy-video');
@@ -61,7 +30,8 @@ function monitorUrlAvailability() {
 
 function setAvailability(src, tagId) {
   fetch(src).then(response => {
-    setAvailable(tagId, response.status === 200);
+    var available = response.status === 200;
+    setAvailable(tagId, available);
   });
 }
 
@@ -79,3 +49,16 @@ function setAvailable(tagId, available) {
 
 monitorUrlAvailability();
 setInterval(monitorUrlAvailability, 5000);
+
+var player = videojs('hls-example');
+function changeSource(src) {
+  player.src({ type: "application/x-mpegURL", src: src });
+
+  if (src === VIDEO_SRC) {
+    $('#playing-title').text('Playing Video');
+  } else {
+    $('#playing-title').text('Playing Audio');
+  }
+}
+
+changeSource(AUDIO_SRC);
